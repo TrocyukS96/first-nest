d7e42ef1a57cef0388a487ef6a8a7e097efcd3bf
TrocyukS96 <trotzuk.stanislav@gmail.com>
2023-03-04 12:58:28 +0300

    created a separate module for products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './products/products.controller';
-import { ProductsService } from "./products/products.service";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ProductsModule } from "./products/products.module";
 
 //@Module - Это декоратор
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController],
-  providers: [AppService,ProductsService],
+  imports: [ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
