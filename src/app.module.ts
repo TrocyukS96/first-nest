6ef9f4e88d7500beabf18cf0bf55206fd1bd89d8
TrocyukS96 <trotzuk.stanislav@gmail.com>
2023-03-04 12:53:30 +0300

    created products.service.ts & injected to products.controller.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
+import { ProductsService } from "./products/products.service";
 
 //@Module - Это декоратор
 @Module({
   imports: [],
   controllers: [AppController, ProductsController],
-  providers: [AppService],
+  providers: [AppService,ProductsService],
 })
 export class AppModule {}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,35 +1,49 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Header, HttpCode, HttpStatus, Param, Post, Put } from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
+import { ProductsService } from "./products.service";
 
-@Controller('products')
+@Controller("products")
 export class ProductsController {
+
+  //делаем инжекцию переменной в конструктор, чтобы через this получить доступ к классу ProductsService
+  constructor(private readonly productsService: ProductsService) {
+  }
+
+  // @Get()
+  // // @Redirect('https://google.com',301)
+  // getAll(@Req() req:Request, @Res() res:Response) {
+  //   res.status(201).end('Poka')
+  //   return 'getAll'
+  // }
 
   @Get()
   getAll() {
-    return 'getAll'
+    return this.productsService.getAll();
   }
 
-  @Get(':id')
+  @Get(":id")
   // декоратор @Param, в переменую params мы оборачиваем то что приходит с декоратора
   //  getOne(@Param() params){
   // 2 способ
-  getOne(@Param('id') id):string {
-    return 'getOne ' + id
+  getOne(@Param("id") id): string {
+    return this.productsService.getById(id);
   }
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto):string {
-    return `Title: ${createProductDto.title} Price:${createProductDto.price}`
+  @HttpCode(HttpStatus.CREATED)
+  @Header("Cache-Control", "none")
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id:string):string{
-    return 'remove ' +id
+  @Delete(":id")
+  remove(@Param("id") id: string): string {
+    return "remove " + id;
   }
 
-  @Put(':id')
-  update(@Body() updateProductDto:UpdateProductDto, @Param('id') id:string):string{
-    return 'Update ' + id
+  @Put(":id")
+  update(@Body() updateProductDto: UpdateProductDto, @Param("id") id: string): string {
+    return "Update " + id;
   }
 }
